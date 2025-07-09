83e2ef5da2993c542429f262bd9c40b8fd0a62b4
MateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-07-09 10:49:19 -0300

    Question e Answer comment

---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,44 @@
+import { QuestionCommentsRepository } from './../repositories/question-comments-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionRepository } from '../repositories/question-repository'
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  content: string
+}
+
+interface CommentOnQuestionUseCaseResponse {
+  questionComment: QuestionComment
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionsrepository: QuestionRepository,
+    private questionCommentsRepository: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    questionId,
+    content,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionsrepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityID(authorId),
+      questionId: new UniqueEntityID(questionId),
+      content,
+    })
+    await this.questionCommentsRepository.create(questionComment)
+
+    return {
+      questionComment,
+    }
+  }
+}
---
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+
+export class InMemoryQuestionCommentsRepository
+  implements QuestionCommentsRepository
+{
+  public items: QuestionComment[] = []
+
+  async create(questionComment: QuestionComment) {
+    this.items.push(questionComment)
+  }
+}
